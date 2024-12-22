65a303ac766821a5131ede5592fdcd39edf828b3
Rohit Sharma <rsantoshsharma9730@gmail.com>
2024-12-22 12:54:08 +0530

    Initial commit - Weather App

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Whether from './Componets/Whether'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Whether/>
+    </div>
+  )
+}
+
+export default App
